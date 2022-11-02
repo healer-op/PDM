33c1846ab2fe1fcbce8879ac45c0305e61ffc537
HEALER <65026164+healer-op@users.noreply.github.com>
2022-11-02 13:25:02 +0530

    Update compact.js

--- js/compact.js
+++ js/compact.js
@@ -6,7 +6,7 @@ fetch('https://script.google.com/macros/s/AKfycbyksyxaz0QORyq_KMeu44XmkCLt2tTwac
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -66,7 +66,7 @@ fetch('https://script.google.com/macros/s/AKfycbwN3pSFosGYLrZ_6OVNtiXLA6MajzNc9c
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -188,7 +188,7 @@ fetch('https://script.google.com/macros/s/AKfycbzH2frGu4ij2GzbVt4v0xabQjsAk6B13b
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -247,7 +247,7 @@ fetch('https://script.google.com/macros/s/AKfycbzjM-HQogzxyBDTR5UNeHUHlawcO_K6kH
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -306,7 +306,7 @@ fetch('https://script.google.com/macros/s/AKfycbyPFZSbxEtd0BQmQ1Ojuk0jXVrEZodBHt
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -365,7 +365,7 @@ fetch('https://script.google.com/macros/s/AKfycbxCnsQ04XGaKrzsZnqrmVxFwLKhCEYy5y
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -424,7 +424,7 @@ fetch('https://script.google.com/macros/s/AKfycbzk1ZSPQf7BO27rweBUIQNq2MG6R27Dl7
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -483,7 +483,7 @@ fetch('https://script.google.com/macros/s/AKfycbxvohQJb4ZeUaI8k-6ulId4ClGvFSn1OU
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
@@ -536,13 +536,72 @@ fetch('https://script.google.com/macros/s/AKfycbxvohQJb4ZeUaI8k-6ulId4ClGvFSn1OU
     document.querySelector("#vans").insertAdjacentHTML("afterbegin", html);
 })
 
+fetch('https://script.google.com/macros/s/AKfycbwpTWEeLHdIQisqXL_dpF9NgQExZ9DAJhg79HGqHDJjDd-Y0bPs3duQf7ERBVUooPKo/exec')
+.then(response =>{
+    if(!response.ok){
+        throw Error("ERROR");
+    }
+    return response.json();})
+.then(datas => {
+    // console.log(data[0]);
+    let length = datas.length;
+    let j=0;
+    let data = [];
+    for(var i=0;i<length;i++){
+        if(datas[i].Stock=="X" || datas[i].Stock=="x"){
+            
+        }
+        else{
+            data[j] = datas[i]
+            j++
+        }
+    }
+    const html = data.map((img, i) =>{
+        return `
+        <div class="col">
+                        <div class="card shadow-sm">
+                            <img class="card-img-top"
+                                src="${data[i].Picture}"
+                                width="100%" height="225">
+                            <div class="card-body">
+                                <p>${data[i].Car_made} <b>${data[i].Model_Name}</b></p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item" style="display:flex"><b>Finance
+                                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:&nbsp</b>
+                                    <p>${data[i].Finance}</p>
+                                </li>
+                                <li class="list-group-item" style="display:flex"><b>Finance 8w&nbsp&nbsp:&nbsp</b>
+                                    <p>${data[i].weeks}</p>
+                                </li>
+                                <li class="list-group-item" style="display:flex"><b>Drivetrain &nbsp&nbsp&nbsp:&nbsp</b>
+                                    <p>${data[i].Drivetrain}</p>
+                                </li>
+                                <li class="list-group-item" style="display:flex"><b>Capacity
+                                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:&nbsp</b>
+                                    <p>${data[i].Capacity}</p>
+                                </li>
+                                <li class="list-group-item" style="display:flex"><b>Engine
+                                        &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:&nbsp</b>
+                                    <p>${data[i].Engine}</p>
+                                </li>
+                            </ul>
+                            <div class="card-footer">
+                                <small class="text"><b>================= ${data[i].Price}</b></small>
+                            </div>
+                        </div>
+                    </div>`;
+    }).join('');
+    document.querySelector("#edm").insertAdjacentHTML("afterbegin", html);
+})
+
 fetch('https://script.google.com/macros/s/AKfycbzRFaTbH03-WBFVEx0P9Mr8NbEyCG2B1Ugvy4qF42yL2m3T-2J_vvQx9zeAqhsO6CMs/exec')
 .then(response =>{
     if(!response.ok){
         throw Error("ERROR");
     }
     return response.json();})
-.then(data => {
+.then(datas => {
     // console.log(data[0]);
     let length = datas.length;
     let j=0;
